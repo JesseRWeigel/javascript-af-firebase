0c4e348c33cc21f6e7f253fb1587543861be3620
Harshit Pant <pantharshit00@gmail.com>
2018-02-11 16:50:32 +0530

    feat(index.js): Add feat generation function

--- functions/index.js
+++ functions/index.js
@@ -19,3 +19,35 @@ exports.makeUser = functions.auth.user().onCreate(event => {
       console.error(err);
     });
 });
+
+exports.generateFeed = functions.firestore
+  .document('posts/{postId}')
+  .onCreate(event => {
+    const data = event.data.data();
+    const pid = event.data.id;
+    return store
+      .collection('users')
+      .doc(data.authorId)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          const data2 = doc.data();
+          const followers = data2.followers;
+          console.log(followers);
+          if (followers.length) {
+            for (let follower of followers) {
+              store
+                .collection('users')
+                .doc(follower)
+                .collection('feed')
+                .doc(pid)
+                .create({});
+            }
+          }
+        }
+        return doc;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
